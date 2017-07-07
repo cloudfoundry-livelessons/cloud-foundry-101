eddfbd2623b2fcb2c79c829e389731720c837bd5
Joshua McKenty <jmckenty@gmail.com>
2017-07-07 14:39:57 -0700

    Basic python app

--- python/hello.py
+++ python/hello.py
@@ -4,8 +4,10 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def hello():
-    return "Hi!"
+    JOKE = os.environ.get('JOKE', "No Joke.")
+    return JOKE
 
 PORT = os.environ.get('PORT', 8080)
 HOST = "0.0.0.0"
+
 app.run(HOST, PORT)
